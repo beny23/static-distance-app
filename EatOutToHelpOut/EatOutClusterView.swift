a399795b6e9231bbc163600f515a1f4c553343f3
paulstringer <pstringer@equalexperts.com>
2020-08-14 13:02:49 +0100

    - Unhardcode Textlabel on Annotation to use system label font size - Remove UIColor.fill which has no effect.

--- EatOutToHelpOut/EatOutClusterView.swift
+++ EatOutToHelpOut/EatOutClusterView.swift
@@ -31,9 +31,8 @@ private extension EatOutClusterView {
         let renderer = UIGraphicsImageRenderer(size: CGSize(width: 40.0, height: 40.0))
         let count = annotation.memberAnnotations.count
         image = renderer.image { _ in
-            UIColor.purple.setFill()
             UIBezierPath(ovalIn: CGRect(x: 0.0, y: 0.0, width: 40.0, height: 40.0)).fill()
-            let attributes = [NSAttributedString.Key.foregroundColor: UIColor.white, NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 20.0)]
+            let attributes = [NSAttributedString.Key.foregroundColor: UIColor.white, NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: UIFont.labelFontSize)]
             let text = "\(count)"
             let size = text.size(withAttributes: attributes)
             let rect = CGRect(x: 20 - size.width / 2, y: 20 - size.height / 2, width: size.width, height: size.height)
